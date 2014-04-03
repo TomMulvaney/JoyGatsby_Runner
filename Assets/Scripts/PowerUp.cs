9acd8a81a1901a0d8aecfb9ec96b1a355ce3d4b8
TomMulvaney <mulvaney.tom@gmail.com>
2014-04-03 21:26:56 +0100

    Created PowerUp script with placeholder logic. PowerUp scripts currently destroys own gameObject on contact with the player.

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUp : MonoBehaviour 
+{
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Player") 
+		{
+			Destroy(gameObject);
+		}
+	}
+}
